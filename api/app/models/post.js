61dffa549bae96feb6eb9564accb7250f640ddb8
JChenoweth <jchenoweth@Jennifers-MBP.local>
2022-11-29 11:05:40 -0600

    adds model post

---
+++ api/app/models/post.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const postSchema = new mongoose.Schema(
+	{
+		game: {
+			type: String,
+			required: true,
+		},
+        mode: {
+			type: String,
+			required: true,
+		},
+        players: {
+			type: Number,
+			required: true,
+		},
+        info: {
+			type: String,
+			required: true,
+		},
+        owner: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	}
+)
+
+module.exports = mongoose.model('Post', postSchema)
